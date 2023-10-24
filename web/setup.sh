72500c71291d723f98fd932b92b5a69bd3b39785
ramed <marf4196@outlook.com>
2023-10-24 17:19:07 +0330

    Deloyment done, Ready for update

--- web/setup.sh
+++ web/setup.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
 #DEB12 OS level
-
+​
 # DEB 12 Startup
 #crontab -e
 #@reboot /usr/bin/sleep 5; /root/setup.sh 2>&1 | tee /tmp/logsetup.txt
 #chmod +x setup.sh
-
+​
 # API KEY
 API_KEY='a3Bc4D5eF6g7H8i9J0kL1mN2oP3'
-
+​
 # creating XML file from VMapp
 vmtoolsd --cmd "info-get guestinfo.ovfenv" > /tmp/ovf_env.xml
 TMPXML='/tmp/ovf_env.xml'
-
+​
 # getting variables from XML 
 DEPLOYMENT=`cat $TMPXML| grep -e deployment |sed -n -e '/value\=/ s/.*\=\" *//p'|sed 's/\"\/>//'`
-
-
+​
+​
 if  [ "$DEPLOYMENT" = "true" ]; then
     #IPV4
     IPV4=`cat $TMPXML| grep -e ip0 |sed -n -e '/value\=/ s/.*\=\" *//p'|sed 's/\"\/>//'`
     GATE4=`cat $TMPXML| grep -e gateway0 |sed -n -e '/value\=/ s/.*\=\" *//p'|sed 's/\"\/>//'`
     SUBNET4=`cat $TMPXML| grep -e netmask0 |sed -n -e '/value\=/ s/.*\=\" *//p'|sed 's/\"\/>//'`
     MAINGATE4=`cat $TMPXML| grep -e maingateway |sed -n -e '/value\=/ s/.*\=\" *//p'|sed 's/\"\/>//'`
-
+​
     # IPV6
     IPV6=`cat $TMPXML| grep -e v6ip0 |sed -n -e '/value\=/ s/.*\=\" *//p'|sed 's/\"\/>//'`
     GATE6=`cat $TMPXML| grep -e v6gateway0 |sed -n -e '/value\=/ s/.*\=\" *//p'|sed 's/\"\/>//'`
     SUBNET6=`cat $TMPXML| grep -e v6netmask0 |sed -n -e '/value\=/ s/.*\=\" *//p'|sed 's/\"\/>//'`
-
+​
     # hostname
     HOSTNAME=`cat $TMPXML| grep -e hostname |sed -n -e '/value\=/ s/.*\=\" *//p'|sed 's/\"\/>//'`
 	DISKRESIZE=`cat $TMPXML| grep -e diskresize |sed -n -e '/value\=/ s/.*\=\" *//p'|sed 's/\"\/>//'`
-
+​
     # ssh public key
-    SSH_PUB=`cat /tmp/ovf_env.xml| grep -e ssh_pub | cut -c 47- | rev | cut -c 4- | rev`
+    SSH_PUB=`cat $TMPXML| grep -e ssh_pub | cut -c 47- | rev | cut -c 4- | rev`
     PASSWORD=`cat $TMPXML| grep -e password |sed -n -e '/value\=/ s/.*\=\" *//p'|sed 's/\"\/>//'`
-
+​
     # Extra IP 1
     EXT_IP1=`cat $TMPXML| grep -e ext1 |sed -n -e '/value\=/ s/.*\=\" *//p'|sed 's/\"\/>//'`
-
+​
     # Extra IP 2
     EXT_IP2=`cat $TMPXML| grep -e ext2 |sed -n -e '/value\=/ s/.*\=\" *//p'|sed 's/\"\/>//'`
-
-
+​
+​
     # network file
     NETWORKFILE="/etc/network/interfaces"
 	
-
+​
     sed -i "s/IPv4/$IPV4/" $NETWORKFILE
     sed -i "s/IPv6/$IPV6/" $NETWORKFILE
     sed -i "s/SUBNET4/$SUBNET4/" $NETWORKFILE
@@ -55,8 +55,8 @@ if  [ "$DEPLOYMENT" = "true" ]; then
     sed -i "s/GATE4/$GATE4/" $NETWORKFILE
     sed -i "s/GATE6/$GATE6/" $NETWORKFILE
 	
-	/etc/init.d/networking restart	
-
+	
+​
     # adding extra ip 1 and 2
     if [ $EXT_IP1 ]; then
         echo 'auto ens192:0' >> $NETWORKFILE
@@ -64,7 +64,7 @@ if  [ "$DEPLOYMENT" = "true" ]; then
         echo "        address $EXT_IP1" >> $NETWORKFILE
         echo "        gateway $GATE4" >> $NETWORKFILE
     
-	    /etc/init.d/networking restart	
+​
     fi
 	
     if [ $EXT_IP1 ] && [ $EXT_IP2 ]; then
@@ -72,15 +72,15 @@ if  [ "$DEPLOYMENT" = "true" ]; then
         echo 'iface ens192:1 inet static' >> $NETWORKFILE
         echo "        address $EXT_IP2" >> $NETWORKFILE
         echo "        gateway $GATE4" >> $NETWORKFILE
-
-	    /etc/init.d/networking restart	
+​
     else
         echo "not valid extra IP"
+	
     fi
-
+	/etc/init.d/networking restart
 		#resize disk
 	if [ "$DISKRESIZE" = "YES" ]; then
-
+​
 		/usr/sbin/parted  /dev/sda mkpart primary ext4 10762 100%
 		/usr/sbin/pvcreate pvcreate /dev/sda3
 		/usr/sbin/vgextend debian12-vg /dev/sda3
@@ -91,14 +91,14 @@ if  [ "$DEPLOYMENT" = "true" ]; then
     ping -c 2 $MAINGATE4
     # check internet connection
 	res=$(ping -c 4 8.8.8.8)
-
+​
 	if [[ $res == *"0% packet loss"* ]] || [[ $res == *"25% packet loss"* ]]; then
 	echo "VM IS ONLINE"
 	else
 		/etc/init.d/networking restart	
 		TRYAGAIN="YES"
 	fi
-
+​
     # check internet connection again
     if  [ "$TRYAGAIN" = "YES" ]; then
 		/usr/sbin/ifup ens192 && /usr/sbin/ifdown ens192 && /usr/sbin/ifup ens192
@@ -116,21 +116,22 @@ if  [ "$DEPLOYMENT" = "true" ]; then
 			exit 1
         fi
     fi
-
-    # update
-    DEBIAN_FRONTEND=noninteractive apt upgrade -y --force-yes -fuy -o Dpkg::Optitons::='--force-confold' 
+	#hostname
+	
 	
     # create ssh key file
     if [[ $SSH_PUB ]]
     then	
         echo $SSH_PUB > /root/.ssh/authorized_keys
+        sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
         #restart ssh service
         /etc/init.d/ssh restart
-
+​
     else
         echo root:$PASSWORD | /usr/sbin/chpasswd
     fi
-	sleep 3
+	
+	sleep 1
     # deleting XML file
     rm -f $TMPXML
     rm -f /root/setup.sh
@@ -138,6 +139,18 @@ if  [ "$DEPLOYMENT" = "true" ]; then
     crontab -r
 fi
 #
+​
+​
+if  [ "$DEPLOYMENT" = "update" ]; then
+​
+​
+​
+    # update
+    DEBIAN_FRONTEND=noninteractive apt upgrade -y --force-yes -fuy -o Dpkg::Optitons::='--force-confold' 
+	
+	
+	
+fi
  #       address IPv4/SUBNET4
   #      gateway GATE4
 #
